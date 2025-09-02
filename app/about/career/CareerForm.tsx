b38e1ede458d511d4bfecf916fbd25e6a6ce4924
R-Coder0 <rishabhpratap008@gmail.com>
2025-09-03 00:43:22 +0530

    changes

--- app/about/career/CareerForm.tsx
+++ app/about/career/CareerForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 
 export default function CareerForm() {
   const [loading, setLoading] = useState(false);
